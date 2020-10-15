e8eb3ca05c2ef55a0373ebca94c866a26289d093
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2020-10-15 14:17:11 -0300

    cambio archivos php

--- enviar.php
+++ enviar.php
@@ -1,48 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Formulario</title> <!-- Aquí va el título de la página -->
+
+</head>
+
+<body>
 <?php
 
-function validar_campo($campo)
-{
-    $campo = trim($campo);
-    $campo = stripcslashes($campo);
-    $campo = htmlspecialchars($campo);
+$Nombre = $_POST['name'];
+$Email = $_POST['email'];
+$telefono = $_POST['phone'];
+$Medio = $_POST['medio'];
+$archivo = $_FILES['file'];
 
-    return $campo;
+if ($Nombre=='' || $Email=='' || $telefono==''){
+
+echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
+
+}else{
+
+
+    require("archivosformulario/class.phpmailer.php");
+    $mail = new PHPMailer();
+
+    $mail->From     = $Email;
+    $mail->FromName = $Nombre; 
+    $mail->AddAddress("blueproducciones.publicidad@gmail.com"); // Dirección a la que llegaran los mensajes.
+   
+// Aquí van los datos que apareceran en el correo que reciba
+    //adjuntamos un archivo 
+        //adjuntamos un archivo
+            
+    $mail->WordWrap = 50; 
+    $mail->IsHTML(true);     
+    $mail->Subject  =  "Un cliente adjunto listado de proveedores";
+    $mail->Body     =  "Nombre: $Nombre \n<br />".    
+    "Email: $Email \n<br />".   
+    "Medio: $Medio \n<br />".   
+    "Teléfono: $telefono \n<br />";
+    $mail->AddAttachment($archivo['tmp_name'], $archivo['name']);
+    
+    
+    if ($mail->Send())
+    echo "<script>alert('Formulario enviado exitosamente, le responderemos lo más pronto posible.');location.href ='javascript:history.back()';</script>";
+    else
+    echo "<script>alert('Error al enviar el formulario');location.href ='javascript:history.back()';</script>";
+
 }
-header('Content-Type: application/json');
-
-
-if( isset($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['email']) && !empty($_POST['medio']) && !empty($_FILES['file']) ){
-
-    $destino = "straticonahuel@gmail.com";
-    $name = validar_campo($_POST["name"]);
-    $phone = validar_campo($_POST["phone"]);
-    $email = validar_campo($_POST["email"]);
-    $msg = validar_campo($_POST["medio"]);
-
-    $ruta = "img/".$_FILES["file"]['name'];
-    move_uploaded_file($_FILES['file']['tmp_name'], $ruta);
-
-
-    $contenido = "Nombre: " .$name;
-    $contenido = "\n phone: " .$phone;
-    $contenido .= "\n Correo: " . $email;
-    $contenido .= "\n medio: " . $msg;
-    $contenido .= $_FILES['file']['name'];
-    
-    $headers = "from: " .$name;
-    
-    $subject = "Un cliente adjunto listado de proveedores, su nomre es: ". $name;
-    
-    $contenido = "Hola un posible cliente completo el formulario de la web. \n\n Nombre del interesado: " .$name ."\n\n Telefono de contacto: ".$phone . "\n\n Correo de contacto: " .$email ."\n\n Su mensaje fue: ".$msg;
-
-    mail($destino,$subject , $contenido, $headers);
-
-
-    return print(json_encode('ok'));
-}
-
-return print(json_encode('no se envio'));
-
-
 
 ?>
+</body>
+</html>
